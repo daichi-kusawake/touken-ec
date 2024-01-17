709ee2e77835ab1c42a0f7d9048025e70c012d2c
Daichi Kusawake <mrm.disney@gmail.com>
2024-01-17 21:52:44 +0900

    seedでのテスト

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Administrator.create(:email_address =>'golfjuice.dev@gmail.com',:password =>'golfjuice')
