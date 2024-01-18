26d7db5fc95691ee79ce29767824ef406679980f
daichi-kusawake <mrm.disney@gmail.com>
2024-01-18 12:01:04 +0900

    コントローラの設定

---
+++ app/controllers/administrators_controller.rb
@@ -0,0 +1,13 @@
+class AdministratorsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+end
---
+++ test/controllers/administrators_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class AdministratorsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get administrators_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get administrators_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get administrators_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get administrators_edit_url
+    assert_response :success
+  end
+end
