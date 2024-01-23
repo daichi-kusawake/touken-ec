6912e2cdfae54adfbdfd42ccd87471c68a9f351c
daichi-kusawake <mrm.disney@gmail.com>
2024-01-23 14:52:53 +0900

    notice

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -25,26 +25,11 @@ class CustomersController < ApplicationController
     #削除機能
     customer = Customer.find(params[:id])
     customer.destroy
-    redirect_to root_path notice:'削除しました'
+    redirect_to root_path
   end
 
-  #新規登録用：Customerのインスタンスを作成
-  def new
-    @customer = Customer.new
-  end
 
-  #登録されたユーザーをDBへ送る
-  def create
-    #インスタンスの生成
-    @customer = Customer.new(customer_params)
-
-    if @customer.save
-      redirect_to root_path, notice:"アカウントを作成しました。"
-    else
-      render :new
-    end
-  end
-
+  private
   def customer_params
     params.require(:customer).permit(:family_name,:last_name,:family_name_furigana,:last_name_furigana,:email_address,:password)
   end
