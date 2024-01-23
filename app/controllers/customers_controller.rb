8a083f3e6c65fbf007f04a9e600683108e76f114
daichi-kusawake <mrm.disney@gmail.com>
2024-01-23 17:35:49 +0900

    turboの修正

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -31,9 +31,25 @@ class CustomersController < ApplicationController
     redirect_to root_path
   end
 
+  #新規登録用：Customerのインスタンスを作成
+  def new
+    @customer = Customer.new
+  end
 
+  #登録されたユーザーをDBへ送る
+  def create
+    #インスタンスの生成
+    @customer = Customer.new(customer_params)
+
+    if @customer.save
+      redirect_to root_path
+    else
+      #turboの場合にバリデーション失敗のエラー表示ができない為、Unprocessable Entityを返すよう指定
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
   def customer_params
     params.require(:customer).permit(:family_name,:last_name,:family_name_furigana,:last_name_furigana,:email_address,:password)
   end
-
 end
