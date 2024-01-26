17189028cf927c19e4ba70a838b00595475b8e06
daichi-kusawake <mrm.disney@gmail.com>
2024-01-26 16:29:13 +0900

    バリデーションの追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,11 +5,22 @@ class Product < ApplicationRecord
   belongs_to :country,optional: true
   belongs_to :era,optional: true
   belongs_to :sign,optional: true
-  belongs_to :japanese_sword,optional: true
 
   #enum設定 1:刀 2:刀装具 3:甲冑
   enum article_id:{japanese_sword:1,tosogu:2,kattyu:3}
 
   #enum設定 0の場合：売り切れ,1の場合販売中
   enum sales_status:{sold_out:0,on_sale:1}
+
+  #バリデーション設定
+  validates :product_name,presence: true
+  validates :description,presence: true
+  validates :article_category,presence: true
+  validates :article_id,presence: true
+  validates :appraisal_document,presence: true
+  validates :country,presence: true
+  validates :era,presence: true
+  validates :sign,presence: true
+  validates :sales_status,presence: true
+
 end
