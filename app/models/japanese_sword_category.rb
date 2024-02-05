b7af634292226857797d91c7d078193c320a06f1
Daichi Kusawake <mrm.disney@gmail.com>
2024-02-05 16:02:56 +0900

    models delete

--- app/models/japanese_sword_category.rb
+++
@@ -1,2 +0,0 @@
-class JapaneseSwordCategory < ApplicationRecord
-end
--- db/migrate/20240117073105_create_article_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateArticleCategories < ActiveRecord::Migration[7.1]
-  def change
-    create_table :article_categories do |t|
-      t.string :article_category_name
-
-      t.timestamps
-    end
-  end
-end
